8bf5babc0e7c89b1cd2e42347948883efc37594e
Donald <donald@firefli.agency>
2019-09-11 19:06:25 -0400

    problems 1-3 completed

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -2,7 +2,27 @@
 # The method should return true if the only common divisor between the two numbers is 1.
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
+def coprime?(num_1, num_2)
+    divisors = Hash.new(0)
+    getFactors(num_1).each { |num| divisors[num] += 1 }
+    getFactors(num_2).each { |num| divisors[num] += 1 }
+    divisors.values.each do |val|
+        if val > 1
+            return false
+        end
+    end
+    true
+end
 
+def getFactors(num)
+    arr = []
+    (2...num).each do |el|
+        if num % el == 0
+            arr << el
+        end
+    end
+    return arr
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
