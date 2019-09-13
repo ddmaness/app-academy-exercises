e0ec865f01a1ea9f366f51629030273b862b8b94
Donald <donald@firefli.agency>
2019-09-13 17:27:12 -0400

    Finish algorithms p-set

--- algorithmic_strategies_exercise/05_compress_str.rb
+++ algorithmic_strategies_exercise/05_compress_str.rb
@@ -2,6 +2,24 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+    compressed = []
+    count = 1;
+    str.split('').each_with_index do |el, idx|
+        if el == str[idx+1]
+            count += 1
+        else
+            if count > 1
+                compressed << count << el
+            else
+                compressed << el
+            end
+            count = 1
+        end
+    end
+    compressed.join('')
+end
+
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
